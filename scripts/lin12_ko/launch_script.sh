c5029a49178131084f28376fcd8d2de5f295c173
r0berta <roberta.bardini@polito.it>
2021-02-12 17:14:04 +0100

    + scripts for non-physiological experimental conditions

---
+++ scripts/lin12_ko/launch_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# $1 number of simulations
+
+rm -rf outpus
+mkdir outputs
+
+counter=1
+while [ $counter -le 100 ]
+do 
+    java -Dde.renew.netPath=../../src/model/lin12_ko_model/ -Dde.renew.classPath=../../src/weka.jar:../.. -jar ../../src/renew2.5.1/loader.jar script simulation_run
+    
+    mv ./cells.csv ./outputs/cells_$counter.csv
+    mv ./cells_unlabeled.arff ./outputs/cells_unlabeled_$counter.arff
+    ((counter++))
+    cat ./fates_collection.csv >> ./outputs/fates.csv
+    rm ./fates_collection.csv
+done
+    
+echo "all done"
